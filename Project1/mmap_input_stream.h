1830b8a6ce49fa22a06a2f8cfc382b3996689e0e
Kasper Nielsen <kasper0406@gmail.com>
2013-09-05 21:35:54 +0200

    Initial code

---
+++ Project1/mmap_input_stream.h
@@ -0,0 +1,14 @@
+//
+//  MMapInputStream.h
+//  IOProject1
+//
+//  Created by Kasper Nielsen on 9/5/13.
+//
+//
+
+#ifndef IOProject1_MMapInputStream_h
+#define IOProject1_MMapInputStream_h
+
+
+
+#endif
