c00292c8a29b93959051a8b7af603f0602897950
Lasse Espeholt <lasse@espeholt.dk>
2013-10-29 20:26:58 +0100

    FStream

--- Project2/f_stream.hpp
+++ Project2/f_stream.hpp
@@ -6,47 +6,71 @@
 #include <cstdint>
 #include <cassert>
 
+#ifdef WIN32
+  #define _fseeki64 _fseeki64
+#else
+  #define _fseeki64 fseek
+#endif
+
 template <typename I>
 class FStream {
 public:
   void open(string filename, uint64_t start, uint64_t end, size_t buffer_size) {
     end_ = end;
-    position_ = start;
+    start_ = start;
 
-    //fd = ::open(filename.c_str(), O_RDWR, S_IRUSR | S_IWUSR);
+    pFile = fopen(filename().c_str(), "w+");
+
+    if (pFile == nullptr) {
+      throw logic_error("Failed to open file");
+    }
+
+    if (_fseeki64(pFile, start * sizeof(I), SEEK_SET) != 0) {
+      throw logic_error("Failed to seek");
+    }
   }
 
   I peek() {
-    assert(position_ < end_);
-    //return buffer_[position_];
-    return 0;
+    I element;
+    if (fread(&element, sizeof(I), 1, pFile) != 1) {
+      throw logic_error("Failed to read from file");
+    }
+    _fseeki64(pFile, -sizeof(I), SEEK_CUR);
+
+    return element;
   }
   
   I read_next() {
-    I res = peek();
-    position_++;
-    return res;
+    I element;
+    if (fread(&element, sizeof(I), 1, pFile) != 1) {
+      throw logic_error("Failed to read from file");
+    }
+
+    return element;
   }
   
   void write(I value) {
-    assert(position_ < end_);
-    //buffer_[position_++] = value;
+    if (fwrite(&value, sizeof(I), 1, pFile) != 1) {
+      throw logic_error("Failed to write");
+    }
   }
   void close() {
-    /*if (::close(fd) == -1) {
-      throw runtime_error("Could not close stream");
-    }*/
+    if (fclose(pFile) != 0) {
+      throw logic_error("Failed to close file");
+    }
   }
   void seek(uint64_t position) {
-    position_ = position;
+    if (_fseeki64(pFile, position * sizeof(I), SEEK_SET) != 0) {
+      throw logic_error("Seek failed");
+    }
   }
   
   bool has_next() {
-    return position_ < end_;
+    return ftell(pFile) / sizeof(I) - 1 < end_;
   }
 
 private:
-  uint64_t position_;
+  uint64_t start_;
   uint64_t end_;
-  int fd;
+  FILE* pFile;
 };
