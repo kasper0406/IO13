f4b7b053368d038f7df67ea5ca74ed7f47e00dd5
Kasper Nielsen <kasper0406@gmail.com>
2013-10-29 14:13:31 +0100

    Simple MMap stream implementation.

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -5,6 +5,7 @@
 #include "test.hpp"
 #include "external_heap.hpp"
 #include "dummy_stream.hpp"
+#include "mmap_stream.hpp"
 
 using namespace std;
 
@@ -72,6 +73,27 @@ void kasper_test() {
    */
 }
 
+void mmap_stream_test() {
+  // MMap stream test
+  MMapStream<uint64_t> stream;
+  
+  // Write some content
+  stream.open("test.bin", 0, 10, 0);
+  for (int i = 0; i < 10; i++)
+    stream.write(i + 1);
+  stream.close();
+  
+  // Read the same content
+  stream.open("test.bin", 0, 10, 0);
+  while (stream.has_next()) {
+    uint64_t peek = stream.peek();
+    uint64_t read = stream.read_next();
+    assert(peek == read);
+    cout << read << endl;
+  }
+  stream.close();
+}
+
 int main(int argc, char *argv[]) {
 #ifdef NDEBUG
   cout << "Release mode" << endl;
@@ -81,8 +103,9 @@ int main(int argc, char *argv[]) {
   
   srand(time(NULL));
 
-  kasper_test();
-  test_swap_sift_test();
-
+  // kasper_test();
+  // test_swap_sift_test();
+  mmap_stream_test();
+  
   return 0;
 }
---
+++ Project2/mmap_stream.hpp
@@ -0,0 +1,82 @@
+#pragma once
+
+#include <cstdint>
+#include <cassert>
+#include <string>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+
+template <typename I>
+class MMapStream {
+public:
+  MMapStream() : fd_(-1), mapped_(nullptr) { }
+  
+  void open(string filename, uint64_t start, uint64_t end, size_t buffer_size) {
+    end_ = end;
+    start_ = start;
+    position_ = start;
+    
+    // Open file. It is ensured by ExternalHeap that the file is big enough.
+    fd_ = ::open(filename.c_str(), O_RDWR, S_IRUSR | S_IWUSR);
+    if (fd_ == -1)
+      throw runtime_error("Failed to open file!");
+    
+    // MMap file into memory
+    const uint64_t elements = end_ - start_;
+    const uint64_t offset = start_;
+    
+    assert(offset % sysconf(_SC_PAGE_SIZE) == 0); // Offset should be multiple of page size
+    mapped_ = (I*) mmap(NULL, sizeof(I) * elements, PROT_READ | PROT_WRITE, MAP_SHARED, fd_, offset);
+    if (mapped_ == MAP_FAILED)
+      throw runtime_error("Failed to map file!");
+  }
+  
+  I peek() {
+    assert(position_ < end_);
+    return mapped_[position_];
+  }
+  
+  I read_next() {
+    I res = peek();
+    position_++;
+    return res;
+  }
+  
+  void write(I value) {
+    assert(position_ < end_);
+    mapped_[position_++] = value;
+  }
+  
+  void close() {
+    if (mapped_ != nullptr) {
+      const uint64_t elements = end_ - start_;
+      if (munmap(mapped_, sizeof(I) * elements) == -1)
+        throw runtime_error("Failed to unmap file!");
+    }
+    
+    if (fd_ != -1) {
+      if (::close(fd_) == -1)
+        throw runtime_error("Failed to close file!");
+    }
+  }
+  
+  void seek(uint64_t position) {
+    assert(position < end_ - start_);
+    position_ = position;
+  }
+  
+  bool has_next() {
+    return position_ < end_;
+  }
+  
+private:
+  uint64_t position_;
+  uint64_t start_;
+  uint64_t end_;
+  
+  int fd_;
+  I* mapped_;
+};
